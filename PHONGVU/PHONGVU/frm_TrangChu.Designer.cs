3832359c439e8fbf0c3ead41cfab3eb43b6d21b0
hieutran92 <hieutran@hhdgroup.com>
2020-08-25 16:21:25 +0700

    thuyet trinh

--- PHONGVU/PHONGVU/frm_TrangChu.Designer.cs
+++ PHONGVU/PHONGVU/frm_TrangChu.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.ForeColor = System.Drawing.Color.DarkBlue;
             this.label7.Location = new System.Drawing.Point(168, 40);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(473, 64);
